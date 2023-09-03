1413ae442f2e802252de88303ce24fb7f0c098a3
damga123 <damian.gaethofs@live.com>
2023-09-03 20:07:51 +0200

    Added git pull keybinding

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -3,4 +3,6 @@ vim.keymap.set("n", "<leader>gcm", ":Git commit -m \"")
 vim.keymap.set("n", "<leader>gd", ":Git diff<CR>")
 vim.keymap.set("n", "<leader>gs", ":Git status<CR>")
 vim.keymap.set("n", "<leader>gps", ":Git push<CR>")
+vim.keymap.set("n", "<leader>gpl", ":Git pull<CR>")
+
 
