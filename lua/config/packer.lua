1c4d56c5bc31ff21ae5152b75702e3ab19079c5e
damga <damian.gaethofs@live.com>
2023-11-29 20:11:12 +0100

    Organized packer file

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -4,105 +4,99 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
 
-  use({ 'rose-pine/neovim', as = 'rose-pine' })
+    -- Apperance
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end
+    }
+    use {"lewis6991/gitsigns.nvim"}
+    use {"folke/which-key.nvim"}
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    -- LSP
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-  use('theprimeagen/harpoon')
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    }
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {                                      -- Optional
-		  'williamboman/mason.nvim',
-		  run = function()
-			  pcall(vim.cmd, 'MasonUpdate')
-		  end,
-	  },
-	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use {
+        'antosha417/nvim-lsp-file-operations',
+        requires = {
+            { "nvim-lua/plenary.nvim" },
+            { "kyazdani42/nvim-tree.lua" },
+        }
+    }
+    use {"mfussenegger/nvim-lint"}
 
-	  -- Autocompletion
-	  {'hrsh7th/nvim-cmp'},     -- Required
-	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	  {'L3MON4D3/LuaSnip'},     -- Required
-  }
-}
+    -- Navigation
+    use('theprimeagen/harpoon')
+    use {"ggandor/leap.nvim"}
+    use {"ggandor/leap-spooky.nvim"}
 
-use {
-	'nvim-tree/nvim-tree.lua',
-	requires = {
-		'nvim-tree/nvim-web-devicons', -- optional
-	},
-	config = function()
-		require("nvim-tree").setup {}
-	end
-}
+    -- Autocompletion
+    use {"mhinz/vim-startify"}
 
-use {"folke/which-key.nvim"}
+    use {"ahmedkhalf/project.nvim"}
 
-use {"mhinz/vim-startify"}
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
+    use {"github/copilot.vim"}
+    use {"windwp/nvim-autopairs"}
 
-use {"ahmedkhalf/project.nvim"}
+    -- Git
+    use {"tpope/vim-fugitive"}
 
-use {
-    'numToStr/Comment.nvim',
-    config = function()
-        require('Comment').setup()
-    end
-}
+    -- Debugging
+    use {"mfussenegger/nvim-dap"}
+    use {"mfussenegger/nvim-dap-python"}
 
-use {
-  'antosha417/nvim-lsp-file-operations',
-  requires = {
-    { "nvim-lua/plenary.nvim" },
-    { "kyazdani42/nvim-tree.lua" },
-  }
-}
-
-use {"codethread/qmk.nvim"}
-
-use {"tpope/vim-repeat"}
-
-use {"tpope/vim-fugitive"}
-
-use {"tpope/vim-surround"}
-
-use {"ggandor/leap.nvim"}
-
-use {"ggandor/leap-spooky.nvim"}
-
-use {"lewis6991/gitsigns.nvim"}
-
-use {"mfussenegger/nvim-lint"}
-
-use {"mfussenegger/nvim-dap"}
-
-use {"mfussenegger/nvim-dap-python"}
-
-use {"airblade/vim-rooter"}
-
-use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-}
-
-use {"windwp/nvim-autopairs"}
-
-use {"github/copilot.vim"}
+    -- Misc
+    use {"codethread/qmk.nvim"}
+    use {"airblade/vim-rooter"}
+    use {"tpope/vim-repeat"}
+    use {"tpope/vim-surround"}
 
 end)
 
