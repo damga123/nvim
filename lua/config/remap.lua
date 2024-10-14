c6bf72c775d4c23c056b3592473e7e77ee9c1ba9
damga123 <damian.gaethofs@live.com>
2024-10-14 12:06:58 +0200

    Add remaps for vim-floaterm

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -22,6 +22,10 @@ vim.keymap.set("n", "<leader>h", "<cmd>split<CR>")
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
-vim.keymap.set('i', '<C-j>', 'copilot#Accept("<CR>")', { expr = true, silent = true})
+vim.keymap.set('i', '<C-j>', 'copilot#Accept("<CR>")', { expr = true, silent = true, replace_keycodes = false})
 
 vim.keymap.set("n", "<leader>ccp", '<cmd>!g++ -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion -Werror -ggdb -pedantic-errors -std=c++20 "%" && a.exe<CR>')
+
+vim.keymap.set("n", "<leader>ft", ':FloatermNew --name=myfloat --height=0.8 --width=0.7 --autoclose=2 nu <CR> ')
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>:q<CR>")
+vim.keymap.set('n', "t", ":FloatermToggle myfloat<CR>")
