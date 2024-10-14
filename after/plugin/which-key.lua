c9cb42ea01b52e427fcb9396d41c834c1c8ed2d5
damga123 <damian.gaethofs@live.com>
2024-10-14 11:53:58 +0200

    Update which-key settings for breaking changes
    The entire plugin was overhauled and I kept getting errors, realised one
    of my file names was off so I missed it when I checked it earlier.
    Removed old config and changed mapping syntax

---
+++ after/plugin/which-key.lua
@@ -0,0 +1,8 @@
+local wk = require("which-key")
+
+wk.add({
+
+    { "<leader>f", group = "find" },
+    { "<leader>ff", desc = "Fuzzy finder" },
+    { "<leader>fp", desc = "Project finder" },
+})
--- lua/config/which-key.lua
+++
@@ -1,9 +0,0 @@
-local wk = require("which-key")
-
-wk.register({
-    f = {
-        name = "find",
-        f = { "Fuzzy finder"},
-        p = { "Project finder"},
-    },
-}, {prefix = "<leader>"})
