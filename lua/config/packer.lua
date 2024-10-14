a540c87a2353a96161fd42fe5be586dfea7c5316
damga123 <damian.gaethofs@live.com>
2024-10-14 11:52:56 +0200

    Add nvim-nio
    This is a dependency for nvim-dap

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -88,6 +88,7 @@ return require('packer').startup(function(use)
     -- Debugging
     use {"mfussenegger/nvim-dap"}
     use {"rcarriga/nvim-dap-ui"}
+    use {"nvim-neotest/nvim-nio"}
     use {"mfussenegger/nvim-dap-python"}
 
     -- Misc
